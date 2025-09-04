0739112bf987b326aab9103ffcab88ecc6c0aabd
Iago Novais <iagonovaisdev@gmail.com>
2025-09-04 20:53:25 -0300

    Added panel certificates module

---
+++ src/panel/pages/certificates/panelCertificatesEdit.jsx
@@ -0,0 +1,167 @@
+import { useContext, useEffect, useState } from 'react';
+import '../../styles/style.scss';
+
+import AppContextProvider from '../../../context/AppContext/AppContextProvider';
+import AuthContextProvider from '../../context/authContext/authContextProvider';
+
+import PanelUp from '../../components/templates/panelUp';
+import PanelBreadcrumb from '../../components/templates/panelBreadcrumb';
+import PanelBottom from '../../components/templates/panelBottom';
+import PanelField from '../../components/templates/panelField';
+import PanelForm from '../../components/templates/panelForm';
+
+import ReturnComponent from '../../../components/returnComponent';
+
+import { PrivateApi } from '../../api/privateApi';
+
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+import { useParams } from 'react-router-dom';
+import { AuthContext } from '../../context/authContext/authContext';
+import PanelFieldImage from '../../components/templates/panelFieldImage';
+
+export default function PanelCertificatesEdit() {
+
+    const {id} = useParams();
+
+    const {auth} = useContext(AuthContext);
+
+    const [loader,setLoader] = useState(false);
+    const [callback,setCallback] = useState({
+        'type': '',
+        'msg': []
+    });
+    const dataModel = {
+        "cover": "",
+        "content": {
+            "pt_br": {
+                "title": "",
+                "description": ""
+            },
+            "en_us": {
+                "title": "",
+                "description": ""
+            }
+        }
+    }
+    const [data,setData] = useState(dataModel);
+    
+    const loadData = async () => {
+
+        if(!id) return;
+        await PrivateApi.get('/certificates/'+id)
+        .then((result)=>{
+            let updateData = {};
+            if( JSON.parse(result.data.response[0].content) ){
+                updateData.content = JSON.parse(result.data.response[0].content);
+            }
+            updateData.cover = result.data.response[0].cover;
+            setData(updateData);
+        })
+        .catch(()=>{
+        });
+
+    };
+    
+    const handleData = (language,e) => {
+        let update = {...data};
+        if(e.target.name == "cover"){
+            update[e.target.name] = e.target.value;
+        } else {
+            update["content"][language][e.target.name] = e.target.value;
+        }
+        setData(update);
+    };
+
+    const handleSubmit = async () => {
+
+        setLoader(true);
+        await PrivateApi.put('/certificates/update/'+id,data,{
+            headers: {
+                'authorization': auth.token
+            }
+        })
+        .then(()=>{
+            setCallback({
+                ...callback,
+                "type": "success",
+                "msg": ["Editado com sucesso"]
+            });
+            setLoader(false);
+        })
+        .catch((e)=>{
+            setCallback({
+                ...callback,
+                "type": "error",
+                "msg": ["Erro ao editar"+e]
+            });
+            setLoader(false);
+        });
+
+    };
+
+    useEffect(()=>{
+        loadData();
+    },[setData]);
+
+    return(
+        <>  
+            <AppContextProvider>
+                <AuthContextProvider>
+                    <div className="panel">
+                        <PanelUp/>
+                        <div className="panel-middle">
+                            <PanelBreadcrumb
+                            content={{
+                                "title":"Editar certificado",
+                                "list":[
+                                    {'title':'Painel','href':'/panel','active':''},
+                                    {'title':'Certificados','href':'/panel/certificates','active':''},
+                                    {'title':'Editar','href':'/panel/certificates/edit/'+id,'active':'true'}
+                                ]
+                            }}
+                            />
+                            
+                            <div className="panel-content">
+                                <div className="container">
+                                    <div className="row">
+                                        <div className="col-md-12">
+
+                                            <Tabs
+                                            defaultActiveKey="pt_br"
+                                            id="manager" 
+                                            >
+                                                <Tab eventKey="pt_br" title="PT-BR">
+                                                    <PanelForm>
+                                                        <ReturnComponent type={callback.type} list={callback.msg}/>
+                                                        <PanelFieldImage language="pt_br" handleData={handleData} name='cover' label='Capa:' value={data.cover}/>
+                                                        <PanelField language="pt_br" handleData={handleData} type='text' name='title' label='Título:' value={data['content']['pt_br'].title}/>
+                                                        <PanelField language="pt_br" handleData={handleData} type='textarea' name='description' label='Descrição:' value={data['content']['pt_br'].description}/>
+                                                        <PanelField submit={handleSubmit} setLoader={setLoader} loader={loader} type='submit' value='Salvar'/>
+                                                    </PanelForm>
+                                                </Tab>
+                                                <Tab eventKey="en_us" title="EN-US">
+                                                    <PanelForm>
+                                                        <ReturnComponent type={callback.type} list={callback.msg}/>
+                                                        <PanelFieldImage language="en_us" handleData={handleData} name='cover' label='Capa:' value={data.cover}/>
+                                                        <PanelField language="en_us" handleData={handleData} type='text' name='title' label='Título:' value={data['content']['en_us'].title}/>
+                                                        <PanelField language="en_us" handleData={handleData} type='textarea' name='description' label='Descrição:' value={data['content']['en_us'].description}/>
+                                                        <PanelField submit={handleSubmit} setLoader={setLoader} loader={loader} type='submit' value='Salvar'/>
+                                                    </PanelForm>
+                                                </Tab>
+                                            </Tabs>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>                           
+
+                        </div>
+                        <PanelBottom/>
+                    </div>
+                </AuthContextProvider>
+            </AppContextProvider>
+        </>
+    );
+
+}

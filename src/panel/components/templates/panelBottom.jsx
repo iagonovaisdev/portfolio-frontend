129b83590b6d3490df1d2860f143534489d96029
Iago Novais <iagonovaisdev@gmail.com>
2025-09-04 20:50:55 -0300

    Added template component panelBottom

---
+++ src/panel/components/templates/panelBottom.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function PanelBottom(){
+
+    return(
+        <div className="panel-bottom">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <span>© Copyright Iago Novais - Todos os direitos reservados</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+
+};

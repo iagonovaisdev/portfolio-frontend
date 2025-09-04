6317e9bfe18f538bc384f18539f1d64525cdc667
Iago Novais <iagonovaisdev@gmail.com>
2025-09-04 20:55:18 -0300

    Added panel projectsCategories module

---
+++ src/panel/pages/projectsCategories/panelProjectsCategories.jsx
@@ -0,0 +1,203 @@
+import { useContext, useEffect, useState } from 'react';
+import '../../styles/style.scss';
+
+import Sortable from 'sortablejs';
+import PanelUp from '../../components/templates/panelUp';
+import PanelBreadcrumb from '../../components/templates/panelBreadcrumb';
+import PanelBottom from '../../components/templates/panelBottom';
+import ReturnComponent from '../../../components/returnComponent';
+import LandingPagination from "../../../landing/components/templates/landingPagination";
+import { AppContext } from '../../../context/AppContext/AppContext';
+import { PrivateApi } from '../../api/privateApi';
+import { AuthContext } from '../../context/authContext/authContext';
+
+export default function PanelProjectsCategories() {
+
+    const { translation } = useContext(AppContext);
+
+    const {auth} = useContext(AuthContext);
+
+    const [data,setData] = useState([]);
+    const [callback,setCallback] = useState({
+        type: '',
+        msg: []
+    });
+    const [ordering,setOrdering] = useState(false);
+
+    const [paginationTotal,setPaginationTotal] = useState(1);
+    const [paginationActual,setPaginationActual] = useState(1);
+    const changePagination = (id) => {
+        setPaginationActual(id);
+        setData([]);
+        getData(paginationActual);
+    };
+
+    const getData = (limit = 15) => {
+        PrivateApi.get('/projects-categories?page='+paginationActual+'&limit='+limit).then((result)=>{
+            let newData = [];
+            result.data.response.map((item)=>{
+                let newItem = {};
+                if( JSON.parse(item.content) ) { 
+                    newItem.content = JSON.parse(item.content);
+                }
+                newItem.id = item._id;
+                newData.push(newItem); 
+                setPaginationTotal(result.data.records.pages);
+            });
+            setData(newData);
+            console.log(newData);
+        }).catch(()=>{
+            console.log("erro");
+        });
+    };
+
+    const deleteData = (id) => {
+        if( confirm('Tem certeza que deseja deletar esse item?') ) {
+            PrivateApi.delete('/projects-categories/'+id,{
+                headers: {
+                    'authorization': auth.token
+                }
+            }).then(()=>{
+                setCallback({
+                    'type': 'success',
+                    'msg': ['Deletado com sucesso']
+                });
+                getData();
+            }).catch((error)=>{
+                console.log(error);
+                setCallback({
+                    'type': 'error',
+                    'msg': ['Erro ao deletar']
+                });
+            });
+        }
+
+    };
+
+    const reorder = () => {
+        setOrdering(!ordering);
+    };
+
+    useEffect(()=>{
+
+        getData();
+
+        const sortableParent = document.querySelector(".sortable tbody");
+        const sortableChildren = "tr";
+        Sortable.create(sortableParent,{
+            handle: '.lni-move',
+            animation: 150,
+            onEnd: () => {
+                
+                let reindex = sortableParent.querySelectorAll(sortableChildren);
+                let newOrder = [];
+                let actual = 0;
+                for(let item of reindex) {
+                    let newItem = {'id':item.id,'order':actual};
+                    newOrder.push(newItem);
+                    actual++;
+                }
+                console.log(newOrder);
+
+                PrivateApi.put('/projects-categories/reorder/',{'order':newOrder},{
+                    headers: {
+                        'authorization': auth.token
+                    }
+                })
+                .then((result)=>{
+                    console.log("Reordered "+result);
+                }).catch(()=>{
+                    console.log("Error to reorder");
+                });
+
+            }
+        })
+
+    },[setData]);
+
+    return(
+        <>  
+            <div className="panel">
+                <PanelUp/>
+                <div className="panel-middle">
+                    <PanelBreadcrumb
+                    content={{
+                        "title":"Projetos (Categorias)",
+                        "list":[
+                            {'title':'Painel','href':'/panel','active':''},
+                            {'title':'Projetos','href':'/panel/projetos','active':''},
+                            {'title':'Categorias','href':'/panel/stack-categories','active':'true'}
+                        ]
+                    }}
+                    />
+
+                    <div className="panel-content">
+                        <div className="container">
+
+                            <div className="row flex flex-middle">
+                                <div className="col-md-6">
+                                    <h1 className="panel-content-title">
+                                        Categorias
+                                    </h1>
+                                </div>
+                                <div className="col-md-6">
+                                    <span onClick={reorder} className={`panel-content-button ${!ordering ? 'panel-content-button-disabled':''} float-right`}>
+                                        Reordenar <i className="lni lni-list"></i>
+                                    </span>
+                                    <a href="/panel/projects/categories/add" className="panel-content-button float-right">
+                                        Adicionar <i className="lni lni-plus"></i>
+                                    </a>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-md-12">
+                                    {callback.msg && <ReturnComponent type={callback.type} list={callback.msg}/>}
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-md-12">
+                                    <table id="sortable" className='tabled sortable'>
+                                        <thead>
+                                            <tr>
+                                                {ordering && <th></th>}
+                                                <th>Título</th>
+                                                <th>Ação</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {data.map((item,i) =>
+                                                <tr id={item.id} key={i}>
+                                                    {ordering && <td><i className="lni lni-move"></i></td>}
+                                                    <td>{item.content[translation.code].title}</td>
+                                                    <td>
+                                                        <div className="tabled-actions float-right">
+                                                            <a title="Editar" href={`/panel/projects/categories/edit/${item.id}`}><i className="lni lni-pencil"></i></a>
+                                                            <a title="Deletar" onClick={()=>deleteData(item.id)}  href="#"><i className="lni lni-trash-can"></i></a>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                            )}
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-md-12">
+                                    <LandingPagination 
+                                    total={paginationTotal} 
+                                    setTotal={setPaginationTotal} 
+                                    actual={paginationActual} 
+                                    setActual={changePagination}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <PanelBottom/>
+            </div>
+        </>
+    );
+
+}
---
+++ src/panel/pages/projectsCategories/panelProjectsCategoriesEdit.jsx
@@ -0,0 +1,161 @@
+import { useContext, useEffect, useState } from 'react';
+import '../../styles/style.scss';
+
+import AppContextProvider from '../../../context/AppContext/AppContextProvider';
+import AuthContextProvider from '../../context/authContext/authContextProvider';
+
+import PanelUp from '../../components/templates/panelUp';
+import PanelBreadcrumb from '../../components/templates/panelBreadcrumb';
+import PanelBottom from '../../components/templates/panelBottom';
+import PanelField from '../../components/templates/panelField';
+import PanelForm from '../../components/templates/panelForm';
+
+import ReturnComponent from '../../../components/returnComponent';
+
+import { PrivateApi } from '../../api/privateApi';
+
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+import { useParams } from 'react-router-dom';
+import { AuthContext } from '../../context/authContext/authContext';
+
+export default function PanelStackCategoryEdit() {
+
+    const {id} = useParams();
+
+    const {auth} = useContext(AuthContext);
+
+    const [loader,setLoader] = useState(false);
+    const [callback,setCallback] = useState({
+        'type': '',
+        'msg': []
+    });
+    const dataModel = {
+        "content": {
+            "pt_br": {
+                "title": ""
+            },
+            "en_us": {
+                "title": ""
+            }
+        }
+    }
+    const [data,setData] = useState(dataModel);
+    
+    const loadData = async () => {
+
+        if(!id) return;
+        await PrivateApi.get('/projects-categories/'+id)
+        .then((result)=>{
+            let updateData = {};
+            if( JSON.parse(result.data.response[0].content) ){
+                updateData.content = JSON.parse(result.data.response[0].content);
+            }
+            updateData.icon = result.data.response[0].icon;
+            setData(updateData);
+        })
+        .catch(()=>{
+        });
+
+    };
+    
+    const handleData = (language,e) => {
+        let update = {...data};
+        if(e.target.name == "icon"){
+            update[e.target.name] = e.target.value;
+        } else {
+            update["content"][language][e.target.name] = e.target.value;
+        }
+        setData(update);
+    };
+
+    const handleSubmit = async () => {
+
+        setLoader(true);
+        await PrivateApi.put('/projects-categories/update/'+id,data,{
+            headers: {
+                'authorization': auth.token
+            }
+        })
+        .then(()=>{
+            setCallback({
+                ...callback,
+                "type": "success",
+                "msg": ["Editado com sucesso"]
+            });
+            setLoader(false);
+        })
+        .catch((e)=>{
+            console.log(e);
+            setCallback({
+                ...callback,
+                "type": "error",
+                "msg": ["Erro ao editar"]
+            });
+            setLoader(false);
+        });
+
+    };
+
+    useEffect(()=>{
+        loadData();
+    },[setData]);
+
+    return(
+        <>  
+            <AppContextProvider>
+                <AuthContextProvider>
+                    <div className="panel">
+                        <PanelUp/>
+                        <div className="panel-middle">
+                            <PanelBreadcrumb
+                            content={{
+                                "title":"Editar categoria",
+                                "list":[
+                                    {'title':'Painel','href':'/panel','active':''},
+                                    {'title':'Projetos','href':'/panel/projects','active':''},
+                                    {'title':'Categorias','href':'/panel/projects/categories','active':''},
+                                    {'title':'Editar','href':'/panel/projects/categories/edit/'+id,'active':'true'}
+                                ]
+                            }}
+                            />
+                            
+                            <div className="panel-content">
+                                <div className="container">
+                                    <div className="row">
+                                        <div className="col-md-12">
+
+                                            <Tabs
+                                            defaultActiveKey="pt_br"
+                                            id="manager" 
+                                            >
+                                                <Tab eventKey="pt_br" title="PT-BR">
+                                                    <PanelForm>
+                                                        <ReturnComponent type={callback.type} list={callback.msg}/>
+                                                        <PanelField language="pt_br" handleData={handleData} type='text' name='title' label='Título:' value={data['content']['pt_br'].title}/>
+                                                        <PanelField submit={handleSubmit} setLoader={setLoader} loader={loader} type='submit' value='Salvar'/>
+                                                    </PanelForm>
+                                                </Tab>
+                                                <Tab eventKey="en_us" title="EN-US">
+                                                    <PanelForm>
+                                                        <ReturnComponent type={callback.type} list={callback.msg}/>
+                                                        <PanelField language="en_us" handleData={handleData} type='text' name='title' label='Title:' value={data['content']['en_us'].title}/>
+                                                        <PanelField submit={handleSubmit} setLoader={setLoader} loader={loader} type='submit' value='Save'/>
+                                                    </PanelForm>
+                                                </Tab>
+                                            </Tabs>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>                           
+
+                        </div>
+                        <PanelBottom/>
+                    </div>
+                </AuthContextProvider>
+            </AppContextProvider>
+        </>
+    );
+
+}

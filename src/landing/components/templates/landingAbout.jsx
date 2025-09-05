d61d4a39030f6cc0ff370f7ee0f5a8f81937d04d
Iago Novais <iagonovaisdev@gmail.com>
2025-09-04 21:06:51 -0300

    Added template component landingAbout

---
+++ src/landing/components/templates/landingAbout.jsx
@@ -0,0 +1,179 @@
+import { useContext, useEffect, useState } from "react";
+import { AppContext } from "../../../context/AppContext/AppContext";
+import { parseContentRecursively,getInnerObject } from "../../../utils/utils";
+import LoadingComponent from "../../../components/loadingComponent";
+import ReturnComponent from "../../../components/returnComponent";
+import { PrivateApi } from "../../../panel/api/privateApi";
+
+export default function LandingAbout() {
+
+    const {translation} = useContext(AppContext);
+    const [loading,setLoading] = useState(false);
+    const [error,setError] = useState([]);
+
+    const [skills,setSkills] = useState([]);
+
+    const [about,setAbout] = useState({
+        "content":{
+            "pt_br": {
+                "name":"",
+                "nationality":"",
+                "graduation":"",
+                "portuguese":"",
+                "english":"",
+                "linkedin":"",
+                "github":"",
+                "youtube":"",
+                "instagram":""
+            },
+            "en_us": {
+                "name":"",
+                "nationality":"",
+                "graduation":"",
+                "portuguese":"",
+                "english":"",
+                "linkedin":"",
+                "github":"",
+                "youtube":"",
+                "instagram":""
+            },
+        }
+    });
+    const loadAbout = async () => {
+        await PrivateApi.get('/profile/')
+        .then((result)=>{
+            let updateData = {};
+            if( JSON.parse(result.data.response[0].content) ){
+                updateData.content = JSON.parse(result.data.response[0].content);
+            }
+            updateData.cover = result.data.response[0].cover;
+            setAbout(updateData);
+        })
+        .catch(()=>{
+        });
+    };
+    const loadSkills = async () => {
+        await PrivateApi.get('/stack-tree/')
+        .then((result)=>{
+            let parsed = parseContentRecursively(result.data.response);
+            setSkills(parsed);
+        })
+        .catch(()=>{
+        });
+    };
+    useEffect( () => {
+        loadSkills();
+        loadAbout();
+    },[]);
+
+    return(
+        <div id="about" className="section about">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="section-title">
+                            <span className="title">{translation.landing.about.title}</span>
+                            <span className="subtitle">{translation.landing.about.subtitle}</span>
+                            <span className="description">{translation.landing.about.description}</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="row auto_height">
+                    <div className="col-md-4">
+                        <div className="me auto_height_item">
+                            <div className="avatar" style={{ backgroundImage: `url(${about.cover})`}}>
+                            </div>
+                            <span className="name">{about.content[translation.code].name}</span>
+                            <div className="details">
+                                {translation.landing.me.info.map( (info,i) =>
+                                    <div key={i} className="detail">
+                                        <strong>{info.name}</strong>
+                                        <span>{about.content[translation.code][info.slug]}</span>
+                                    </div>
+                                )}
+                            </div>
+                            <div className="social">
+                                {about.content[translation.code].linkedin && <a title="Linkedin" href={about.content[translation.code].linkedin} target="_blank"><i className="lni lni-linkedin"></i></a>}
+                                {about.content[translation.code].github && <a title="Github" href={about.content[translation.code].github} target="_blank"><i className="lni lni-github"></i></a>}
+                                {about.content[translation.code].youtube && <a title="Youtube" href={about.content[translation.code].youtube} target="_blank"><i className="lni lni-youtube"></i></a>}
+                                {about.content[translation.code].instagram && <a title="Instagram" href={about.content[translation.code].instagram} target="_blank"><i className="lni lni-instagram"></i></a>}
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div className="col-md-8">
+                        {skills && Object.entries(skills).map(([key, top],i) => (
+                            <div className="box" key={key}>
+                                <details open="open">
+                                    <summary className="box-title">
+                                        <i className={top.icon}></i>
+                                        <span>{top.content[translation.code].title}</span>
+                                        <i className="summary-arrow lni lni-chevron-down"></i>
+                                    </summary>
+                                    <div className="box-content">
+                                    {top.list && Object.entries(top.list).map(([subkey, skillWrapper]) => {
+                                        const skill = getInnerObject(skillWrapper);
+                                        return (
+                                            <div key={subkey} className="list-badges top-level">
+                                            <div className="list-badges-item">
+                                                <div className="info">
+                                                <div className="name">
+                                                    <span style={{ borderColor: skill.color }}>
+                                                    {skill.content?.[translation.code]?.title || 'Sem título'}
+                                                    </span>
+                                                </div>
+                                                {skill.content[translation.code].subtitle &&
+                                                    <strong>{skill.content?.[translation.code]?.subtitle}</strong>
+                                                }
+                                                {skill.content[translation.code].description &&
+                                                    <span className="description">
+                                                        {skill.content[translation.code].description}
+                                                    </span>
+                                                }
+                                                </div>
+                                                {skill.list && Object.entries(skill.list).map(([key, subSkillWrapper]) => {
+                                                    const subSkill = getInnerObject(subSkillWrapper);
+                                                    return (
+                                                        <div key={key} className="list-badges">
+                                                        <div className="list-badges-item">
+                                                            <div className="info">
+                                                            <div className="name">
+                                                                <span style={{ borderColor: subSkill.color }}>
+                                                                    {subSkill.content?.[translation.code]?.title || 'Sem título'}
+                                                                </span>
+                                                            </div>
+                                                            {subSkill.content[translation.code].subtitle &&
+                                                                <strong>{subSkill.content?.[translation.code]?.subtitle}</strong>
+                                                            }
+                                                            {subSkill.content[translation.code].description &&
+                                                                <span className="description">
+                                                                    {subSkill.content[translation.code].description}
+                                                                </span>
+                                                            }
+                                                            </div>
+                                                        </div>
+                                                        </div>
+                                                    );
+                                                })}
+                                            </div>
+                                            </div>
+                                        );
+                                        })}
+
+                                    </div>
+                                </details>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-12">
+                        <LoadingComponent name={`${translation.landing.loading}...`} status={loading}/>
+                        <ReturnComponent type="error" list={error}/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+
+}

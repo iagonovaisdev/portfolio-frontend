d37354648da5a342f258181a4b6422d8acab6993
Iago Novais <iagonovaisdev@gmail.com>
2025-09-04 21:05:47 -0300

    Created landing home page structure

---
+++ src/landing/landing.jsx
@@ -0,0 +1,39 @@
+import React, { useContext, useEffect, useState } from 'react';
+import './styles/style.scss';
+
+import { AppContext } from '../context/AppContext/AppContext';
+import AppContextProvider from '../context/AppContext/AppContextProvider';
+
+import LandingHome from './components/templates/landingHome';
+import LandingTop from './components/templates/landingTop';
+import LandingAbout from './components/templates/landingAbout';
+import LandingRoadmap from './components/templates/landingRoadmap';
+import LandingExperience from './components/templates/landingExperience';
+import LandingPractice from './components/templates/landingPractice';
+import LandingNumbers from './components/templates/landingNumbers';
+import LandingProjects from './components/templates/landingProjects';
+import LandingMore from './components/templates/landingMore';
+import LandingContact from './components/templates/landingContact';
+import LandingRdp from './components/templates/landingRdp';
+
+export default function Landing() {
+
+    return(
+        <>  
+            <AppContextProvider>
+                <LandingHome/>
+                <LandingTop/>
+                <LandingAbout/>
+                <LandingRoadmap/>
+                <LandingExperience/>
+                <LandingNumbers/>
+                {<LandingPractice/>}
+                <LandingMore/>
+                <LandingProjects/>
+                <LandingContact/>
+                <LandingRdp/>
+            </AppContextProvider>
+        </>
+    );
+
+};
